8796d5d4d3b76971bbeed3244562c3a1a357d6a2
Katieum <eaglenyoni@gmail.com>
2025-09-03 00:44:13 +0200

    Images need to be in public with image url havin '/' + ...

--- ecom/app/ProductList.tsx
+++ ecom/app/ProductList.tsx
@@ -6,7 +6,7 @@ export default function ProductList({products }:{products: Product[] }) {
         <div>
             {products.map(product => (
                 <div key = {product.id}> 
-                    <Image src={product.imageURL} width={150} height={150} alt="Product Image" />
+                    <Image src={'/' + product.imageURL} width={150} height={150} alt="Product Image" />
                     <h2>{product.name}</h2>
                     <p>R{product.price}</p>
                 </div>
--- ecom/app/products/page.tsx
+++ ecom/app/products/page.tsx
@@ -1,3 +1,11 @@
+import ProductList from "../ProductList";
+import { products } from "../product-data";
+
 export default function ProductsPage(){
-    return <h1>Products Page</h1>
+    return (
+        <>
+            <h1>Products</h1>
+            <ProductList products={products} />
+        </>
+    );
 }
