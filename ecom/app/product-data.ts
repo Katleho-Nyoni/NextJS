8a675acccc9b418c86e65e16838adc6c0fecd897
Katieum <eaglenyoni@gmail.com>
2025-09-03 00:19:08 +0200

    Adding the name property

--- ecom/app/product-data.ts
+++ ecom/app/product-data.ts
@@ -1,5 +1,6 @@
 export interface Product {
     id: string;
+    name: string;
     imageURL: string;
     description: string;
     price: number;
@@ -7,21 +8,25 @@ export interface Product {
 
 export const products: Product [] = [{
     id: "100",
+    name: "Seanamarena-Poone Blanket",
     imageURL: "blanket.jpeg",
     description: "The well celebrated Seanamarena-Poone blanket",
     price: 29.99
 },{
     id: "101",
+    name: "Asus VivoBook Go",
     imageURL: "laptop.webp",
     description: "A high-performance laptop for all your needs",
     price: 34.99
 },{
     id: "102",
+    name: "Black Premium Cast Iron Pots",
     imageURL: "pots.webp",
     description: "A set of ceramic pots for your plants",
     price: 19.99
 },{
     id: "103",
+    name: "Apple iPhone 16 Pro",
     imageURL: "phone.webp",
     description: "The latest smartphone with cutting-edge features",
     price: 39.99
