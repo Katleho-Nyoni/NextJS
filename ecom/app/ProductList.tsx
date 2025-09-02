80d21ca3440545a023fd2269dbd72b455ba07afc
Katieum <eaglenyoni@gmail.com>
2025-09-03 00:36:48 +0200

    Template skeleton

---
+++ ecom/app/ProductList.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Product } from "./product-data";
+
+export default function ProductList({products }:{products: Product[] }) {
+    return (
+        <div>
+            {products.map(product => (
+                <div key = {product.id}> 
+                    <Image src={product.imageURL} width={150} height={150} alt="Product Image" />
+                    <h2>{product.name}</h2>
+                    <p>R{product.price}</p>
+                </div>
+            )
+
+            )}
+        </div>
+    )
+}
