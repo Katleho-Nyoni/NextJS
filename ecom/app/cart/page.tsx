fdc3ee3a4c444a669c5b743b7701d36278121d57
Katieum <eaglenyoni@gmail.com>
2025-09-03 20:35:27 +0200

    Ya tlhakana

--- ecom/app/cart/page.tsx
+++ ecom/app/cart/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+import { useState } from "react";
+import { products } from "../product-data";
+import Link from "next/link";
+
 export default function CartPage(){
-    return <h1>Cart Page</h1>
+    const [cartIDs] = useState(['100','101']);
+    const cartProducts = cartIDs.map(id => products.find(p => p.id === id
+    )!);
+    return (
+        <>
+        <h1>Cart Page</h1>
+        {cartProducts.map(product =>(
+            <Link key = {product.id} href={"/products/" + product.id}>
+                <h3> {product.name} </h3>
+                <p>R{product.price}</p>
+            </Link>
+        ))}
+        </>
+    );
 }
