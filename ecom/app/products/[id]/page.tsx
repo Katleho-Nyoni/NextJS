66797ea40a6df0465c3d7ab8c87a4602a7c5443b
Katieum <eaglenyoni@gmail.com>
2025-09-03 23:46:44 +0200

    Tailwind Styling

--- ecom/app/products/[id]/page.tsx
+++ ecom/app/products/[id]/page.tsx
@@ -3,11 +3,16 @@ import {products} from "@/app/product-data";
 export default function ProductDetailPage({params } : { params: {id: string}}){
     const product = products.find(p => p.id === params.id);
     return (
-        <>
-        <h1>{product!.name}</h1>
-        <p>R{product!.price}</p>
-        <h3>Description</h3>
-        <p>{product!.description}</p>
-        </>
+        <div className="container mx-auto p-8 flex flex-col md:flex-row">
+            <div className="md:w-1/2 mb-4 md:mb-0 md:mr-8">
+                <img src={'/' + product.imageURL} alt="Product Image" className="w-full h-auto rounded-lg shadow-md" />
+            </div>
+            <div className="md:w-1/2">
+                <h1 className="text-4xl font-bold mb-4" >{product!.name}</h1>
+                <p className="text-2xl text-gray-600 mb-6" >R{product!.price}</p>
+                <h3 className="text-2xl font-semibold mb-2" >Description</h3>
+                <p className="text-gray-700" >{product!.description}</p>
+            </div>
+        </div>
     );
 }
