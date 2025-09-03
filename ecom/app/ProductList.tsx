0822f0f15f6de81ab803e73b20e82e7218df7644
Katieum <eaglenyoni@gmail.com>
2025-09-03 19:35:48 +0200

    Updating link to be dynamic

--- ecom/app/ProductList.tsx
+++ ecom/app/ProductList.tsx
@@ -6,7 +6,8 @@ export default function ProductList({products }:{products: Product[] }) {
     return (
         <div>
             {products.map(product => (
-                <Link key = {product.id} href="/product-detail"> 
+                <Link key = {product.id} href=
+                {"/products/" + product.id} > 
                     <Image src={'/' + product.imageURL} width={150} height={150} alt="Product Image" />
                     <h2>{product.name}</h2>
                     <p>R{product.price}</p>
