646763719acc796300855e6194598ab75e5d71a3
Katieum <eaglenyoni@gmail.com>
2025-09-09 01:27:12 +0200

    Post Route Handler example

---
+++ ecom/app/api/hello/route.ts
@@ -0,0 +1,12 @@
+export async function GET(){
+    return new Response('Hello From Next.js route handler!',{
+        status: 200,
+    });
+
+}
+
+export async function POST(){
+    return new Response('POST from Next.js Route Handler!',{
+        status: 200,
+    });
+}
--- ecom/app/hello/route.ts
+++
@@ -1,4 +0,0 @@
-export async function GET(){
-    return new Response('Hello From Next.js route handler!');
-    
-}
