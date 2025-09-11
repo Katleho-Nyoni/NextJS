59b4a2156c3a3f11c53f1881d7f33fb7d47b4c0f
Katieum <eaglenyoni@gmail.com>
2025-09-11 16:48:34 +0200

    Database is conected but i get a 500 status code

--- ecom/app/api/db.ts
+++ ecom/app/api/db.ts
@@ -1,7 +1,12 @@
+import { MongoClient, Db, ServerApiVersion } from 'mongodb';
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
+let cachedClient : MongoClient | null = null;
+let cachedDb : Db | null = null;
 
 export async function connectToDatabase(){
+    if (cachedClient && cachedDb){
+        return {client: cachedClient,db: cachedDb};
+    }
 
     const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rbtalbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -13,9 +18,12 @@ export async function connectToDatabase(){
     }
     });
 
-    try {
-        await client.connect();
-    } finally {
-        await client.close();
-    }
+
+    await client.connect();
+
+    cachedClient = client;
+    cachedDb = client.db('ecom-nextjs');
+
+    return {client, db: client.db('ecom-nextjs') };
+
 }
