518b6ef57b6a4811eabf66ead11bc3cdcb66f3f4
Katieum <eaglenyoni@gmail.com>
2025-09-14 20:57:04 +0200

    Creating the initial state of the cart

--- ecom/app/cart/shoppingCartList.tsx
+++ ecom/app/cart/shoppingCartList.tsx
@@ -7,8 +7,6 @@ import Link from "next/link";
 export default function ShoppingCartList({ initialCartProducts }: {initialCartProducts: Product[] }) {
     const [cartProducts] = useState(initialCartProducts);
 
-    const cartProducts = cartIDs.map(id => products.find(p => p.id === id
-    )!);
     return (
         <>
         <h1>Cart Page</h1>
