4249eaf49e68eaa318170cdbb40dc48f09f4f36c
Katieum <eaglenyoni@gmail.com>
2025-09-16 10:46:48 +0200

    updating links

--- ecom/app/cart/page.tsx
+++ ecom/app/cart/page.tsx
@@ -2,7 +2,7 @@ import ShoppingCartList from "./shoppingCartList";
 
 
 export default async function CartPage(){
-    const response = await fetch('http://localhost:3000/api/users/2/cart/');
+    const response = await fetch('https://wmrrt5qw-3000.euw.devtunnels.ms/api/users/2/cart/');
     const cartProducts = await response.json;
 
     return(
--- ecom/app/cart/shoppingCartList.tsx
+++ ecom/app/cart/shoppingCartList.tsx
@@ -5,7 +5,19 @@ import { Product } from "../product-data";
 import Link from "next/link";
 
 export default function ShoppingCartList({ initialCartProducts }: {initialCartProducts: Product[] }) {
-    const [cartProducts] = useState(initialCartProducts);
+    const [cartProducts, setCartProducts] = useState(initialCartProducts);
+
+    async function removeFromCart(productID: string){
+        const response = fetch('https://wmrrt5qw-3000.euw.devtunnels.ms/api/users/2/cart', {
+            method: 'DELETE',
+            body: JSON.stringify({productID}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const updatedCartProducts = await response.json();
+        setCartProducts(updatedCartProducts);
+    }
 
     return (
         <>
@@ -14,6 +26,11 @@ export default function ShoppingCartList({ initialCartProducts }: {initialCartPr
             <Link key = {product.id} href={"/products/" + product.id}>
                 <h3> {product.name} </h3>
                 <p>R{product.price}</p>
+                <button onClick={(e) => {
+                        e.preventDefault();
+                        removeFromCart(product.id);
+                    }} >- Cart
+                </button>
             </Link>
         ))}
         </>
