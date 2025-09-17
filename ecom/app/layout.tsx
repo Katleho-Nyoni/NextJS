02005fb66ac23cd425692e9db530a41dd7fb2e3c
Katieum <eaglenyoni@gmail.com>
2025-09-17 15:12:11 +0200

    Copy paste to add layout

--- ecom/app/layout.tsx
+++ ecom/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "./NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,9 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <NavBar />
         {children}
       </body>
     </html>
